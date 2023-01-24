c3fcd5872e434ff2742569421296322c0205d1e0
Neto.Changes <infero-sky@yandex.ru>
2023-01-25 00:10:46 +0300

    FinalModification

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
-        double weight = 80;
+        double weight = 77;
         double height = 1.78;
         double myIndex = service.calculate(weight, height);
         System.out.println(myIndex);
