f57a77cc0b951c9c6879ecedd874a112050f96a3
Neto.Changes <infero-sky@yandex.ru>
2023-01-24 17:31:32 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        BmiService service = new BmiService();
+        double weight = 80;
+        double height = 1.78;
+        double myIndex = service.calculate(weight, height);
+        System.out.println(myIndex);
+
+    }
+}
